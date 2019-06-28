f6b37476a0e784f80450e59f77003e1dceedeb27
Colin Turner <colinturner34@gmail.com>
2019-06-28 14:37:30 -0400

    Updates

--- precache-manifest.4c14cc63b84e3c938f3c657841b15a9b.js
+++ precache-manifest.4c14cc63b84e3c938f3c657841b15a9b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e6a8fdca66a700741b7562cb0a13ccc0",
+    "revision": "edf1192a1baeb52bebf5ede58a1b180e",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.510409cd.chunk.css"
   },
   {
-    "revision": "bb4e194605a723dab82e",
+    "revision": "fd1b650cf456ea4d10d1",
     "url": "/static/css/main.586939a1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.673a8e38.chunk.js"
   },
   {
-    "revision": "bb4e194605a723dab82e",
-    "url": "/static/js/main.79c5eb99.chunk.js"
+    "revision": "fd1b650cf456ea4d10d1",
+    "url": "/static/js/main.2145db79.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.789fa34118aca4e11eab1637a36ca0e0.js"
+  "/precache-manifest.4c14cc63b84e3c938f3c657841b15a9b.js"
 );
 
 self.addEventListener('message', (event) => {
