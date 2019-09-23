ef2778a921987656bd25599c1b81572194f3da37
Colin Turner <colinturner34@gmail.com>
2019-09-22 19:41:41 -0700

    add styled-components to ChooseLanguagePage

--- src/components/pages/ChooseLanguagePage/ChooseLanguagePage.js
+++ src/components/pages/ChooseLanguagePage/ChooseLanguagePage.js
@@ -1,25 +1,38 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { Container, Row, Button } from "react-bootstrap";
 import "./ChooseLanguagePage.css";
 import norwayFlag from "../../../assets/norwayFlag.png";
 import swedenFlag from "../../../assets/swedenFlag.png";
 import denmarkFlag from "../../../assets/denmarkFlag.png";
-
+import styled from "styled-components";
 class ChooseLanguagePage extends Component {
   render() {
     const Header = () => (
-      <Fragment>
+      <>
         <h1>Let's begin.</h1>
         <h3>What are you learning?</h3>
-      </Fragment>
+      </>
     );
 
-    const Choice = props => (
-      <Button
+    const StyledButton = styled(Button)`
+      border-radius: 40px;
+      margin: 10px;
+    `;
+
+    const Language = styled.div`
+      font-family: Bree Serif;
+      font-size: 20px;
+    `;
+
+    const Quip = styled.div`
+      font-family: Roboto;
+    `;
+
+    const FlagChoice = props => (
+      <StyledButton
         disabled={!props.available}
         href="#learn"
         variant="light"
-        style={{ borderRadius: "40px", margin: "10px" }}
         className="flagButton"
       >
         <img
@@ -27,39 +40,33 @@ class ChooseLanguagePage extends Component {
           src={props.flag}
           alt={`${props.flagName} flag`}
         />
-        <div style={{ fontFamily: "Bree Serif", fontSize: "20px" }}>
-          {props.language}
-        </div>
-        {props.available ? (
-          <div style={{ fontFamily: "Roboto" }}>{props.quip}</div>
-        ) : (
-          <div style={{ fontFamily: "Roboto" }}>
-            Under development! Check back soon.
-          </div>
-        )}
-      </Button>
+        <Language>{props.language}</Language>
+        <Quip>
+          {props.available ? props.quip : "Under development! Check back soon."}
+        </Quip>
+      </StyledButton>
     );
 
     return (
-      <Fragment>
+      <>
         <Header />
         <Container>
           <Row>
-            <Choice
+            <FlagChoice
               flag={norwayFlag}
               flagName="Norwegian"
               language="Norwegian"
               available={true}
               quip="A fine choice."
             />
-            <Choice
+            <FlagChoice
               flag={swedenFlag}
               flagName="Swedish"
               language="Swedish"
               available={false}
               quip="How grand."
             />
-            <Choice
+            <FlagChoice
               flag={denmarkFlag}
               flagName="Danish"
               language="Danish"
@@ -68,7 +75,7 @@ class ChooseLanguagePage extends Component {
             />
           </Row>
         </Container>
-      </Fragment>
+      </>
     );
   }
 }
