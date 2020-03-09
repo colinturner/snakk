51bd576744d49128a2b0f703c3c8353d86a88395
Colin Turner <colinturner34@gmail.com>
2020-03-08 21:32:56 -0700

    refactor(VerbsPage): make functional

--- src/constants/variables.js
+++ src/constants/variables.js
@@ -3,3 +3,24 @@ export const allCategories = ["present", "past", "present-perfect", "english"];
 export const norwegianCategories = ["present", "past", "present-perfect"];
 
 export const translationLanguage = "english";
+
+const size = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px"
+};
+
+export const device = {
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+  desktopL: `(min-width: ${size.desktop})`
+};
